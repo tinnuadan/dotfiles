5875a30dde9c3058d768305572a44c069304bf82
Tinnuadan <tinnuadan@gmail.com>
2025-07-22 12:05:40 +0200

    lsp adaptions

--- nvim/.config/nvim/lua/kickstart/init-plugins/lsp.lua
+++ nvim/.config/nvim/lua/kickstart/init-plugins/lsp.lua
@@ -1,191 +1,196 @@
 return {
-  -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
+	-- LSP Plugins
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		'folke/lazydev.nvim',
+		ft = 'lua',
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+			},
+		},
+	},
 
-  {
-    -- Main LSP Configuration
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      { 'j-hui/fidget.nvim', opts = {} },
-    },
-    config = function()
-      local lspconfig = require('lspconfig')
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		-- Main LSP Configuration
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			'hrsh7th/cmp-nvim-lsp',
+			{ 'j-hui/fidget.nvim', opts = {} },
+		},
+		config = function()
+			local lspconfig = require('lspconfig')
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      vim.diagnostic.config {
-        severity_sort = true,
-        float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '󰅚 ',
-            [vim.diagnostic.severity.WARN] = '󰀪 ',
-            [vim.diagnostic.severity.INFO] = '󰋽 ',
-            [vim.diagnostic.severity.HINT] = '󰌶 ',
-          },
-        } or {},
-        virtual_text = {
-          source = 'if_many',
-          spacing = 2,
-          format = function(diagnostic)
-            return diagnostic.message
-          end,
-        },
-      }
+			vim.diagnostic.config {
+				severity_sort = true,
+				float = { border = 'rounded', source = 'if_many' },
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				signs = vim.g.have_nerd_font and {
+					text = {
+						[vim.diagnostic.severity.ERROR] = '󰅚 ',
+						[vim.diagnostic.severity.WARN] = '󰀪 ',
+						[vim.diagnostic.severity.INFO] = '󰋽 ',
+						[vim.diagnostic.severity.HINT] = '󰌶 ',
+					},
+				} or {},
+				virtual_text = {
+					source = 'if_many',
+					spacing = 2,
+					format = function(diagnostic)
+						return diagnostic.message
+					end,
+				},
+			}
 
-      local on_attach = function(client, bufnr)
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
-        end
+			local on_attach = function(client, bufnr)
+				local map = function(keys, func, desc, mode)
+					mode = mode or 'n'
+					vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+				end
 
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('go', '<cmd>LspClangdSwitchSourceHeader<CR>', '[G]oto Header/Source')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>fs', require('telescope.builtin').lsp_document_symbols, '[F]ind [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+				map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+				map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+				map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+				map('go', '<cmd>ClangdSwitchSourceHeader<CR>', '[G]oto Header/Source')
+				map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+				map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+				map('<leader>fs', require('telescope.builtin').lsp_document_symbols, '[F]ind [S]ymbols')
+				map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+				map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+				map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+				map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-        if vim.lsp.inlay_hint and client.supports_method('textDocument/inlayHint') then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
-          end, '[T]oggle Inlay [H]ints')
-        end
+				if vim.lsp.inlay_hint and client.supports_method('textDocument/inlayHint') then
+					map('<leader>th', function()
+						vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
+					end, '[T]oggle Inlay [H]ints')
+				end
 
-        if client.supports_method('textDocument/documentHighlight') then
-          local group = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            group = group,
-            buffer = bufnr,
-            callback = vim.lsp.buf.document_highlight,
-          })
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            group = group,
-            buffer = bufnr,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end
+				if client.supports_method('textDocument/documentHighlight') then
+					local group = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+						group = group,
+						buffer = bufnr,
+						callback = vim.lsp.buf.document_highlight,
+					})
+					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						group = group,
+						buffer = bufnr,
+						callback = vim.lsp.buf.clear_references,
+					})
+				end
+			end
 
-      local servers = {
-        clangd = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = { callSnippet = 'Replace' },
-            },
-          },
-        },
-      }
+			local servers = {
+				clangd = {
+					cmd = { 'clangd', '--pretty', '--clang-tidy', '--log=verbose' },
+					init_options = {
+						fallbackFlags = { '-std=c++17' },
+					},
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = { callSnippet = 'Replace' },
+						},
+					},
+				},
+			}
 
-      for name, opts in pairs(servers) do
-        opts.capabilities = capabilities
-        opts.on_attach = on_attach
-        lspconfig[name].setup(opts)
-      end
-    end,
-  },
+			for name, opts in pairs(servers) do
+				opts.capabilities = capabilities
+				opts.on_attach = on_attach
+				lspconfig[name].setup(opts)
+			end
+		end,
+	},
 
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>mf',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        local disabled = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_format = disabled[vim.bo[bufnr].filetype] and 'never' or 'fallback',
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-      },
-    },
-  },
+	{
+		'stevearc/conform.nvim',
+		event = { 'BufWritePre' },
+		cmd = { 'ConformInfo' },
+		keys = {
+			{
+				'<leader>mf',
+				function()
+					require('conform').format { async = true, lsp_format = 'fallback' }
+				end,
+				mode = '',
+				desc = '[F]ormat buffer',
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				local disabled = { c = true, cpp = true }
+				return {
+					timeout_ms = 500,
+					lsp_format = disabled[vim.bo[bufnr].filetype] and 'never' or 'fallback',
+				}
+			end,
+			formatters_by_ft = {
+				lua = { 'stylua' },
+			},
+		},
+	},
 
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-      },
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-    },
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
+	{
+		'hrsh7th/nvim-cmp',
+		event = 'InsertEnter',
+		dependencies = {
+			{
+				'L3MON4D3/LuaSnip',
+				build = (function()
+					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+						return
+					end
+					return 'make install_jsregexp'
+				end)(),
+			},
+			'saadparwaiz1/cmp_luasnip',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-nvim-lsp-signature-help',
+		},
+		config = function()
+			local cmp = require 'cmp'
+			local luasnip = require 'luasnip'
+			luasnip.config.setup {}
 
-      cmp.setup {
-        snippet = {
-          expand = function(args) luasnip.lsp_expand(args.body) end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = 'lazydev',                group_index = 0 },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'nvim_lsp_signature_help' },
-        },
-      }
-    end,
-  },
+			cmp.setup {
+				snippet = {
+					expand = function(args) luasnip.lsp_expand(args.body) end,
+				},
+				completion = { completeopt = 'menu,menuone,noinsert' },
+				mapping = cmp.mapping.preset.insert {
+					['<C-n>'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-y>'] = cmp.mapping.confirm { select = true },
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-l>'] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { 'i', 's' }),
+					['<C-h>'] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { 'i', 's' }),
+				},
+				sources = {
+					{ name = 'lazydev',                group_index = 0 },
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+					{ name = 'path' },
+					{ name = 'nvim_lsp_signature_help' },
+				},
+			}
+		end,
+	},
 }
