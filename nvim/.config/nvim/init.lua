41a2e8b82b1df682c330258b339e63b7692d381d
Tinnuadan <tinnuadan@gmail.com>
2026-03-01 08:30:23 +0100

    option for local config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,8 @@ require 'config.keymap'
 require 'config.custom'
 
 require 'kickstart.init'
+
+local project_config = vim.fn.getcwd() .. "./.nvim.lua"
+if vim.fn.filereadable(project_config) == 1 then
+	dofile(project_config)
+end
